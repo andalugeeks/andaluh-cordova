15a7ba063159136d4a11a0168b5f999372ffe97c
Eduardo Amador Patón <eamadorpaton@gmail.com>
2019-02-26 09:05:57 +0100

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-declare var device;
 
 @Component({
   selector: 'app-root',
@@ -13,30 +12,5 @@ export class AppComponent implements OnInit {
     translate.setDefaultLang('and');
   }
 
-  ngOnInit() {
-    document.addEventListener("deviceready", () => { 
-      // alert(device.platform); 
-    }, false); 
-
-    window.addEventListener('keyboardWillHide', function () {
-      // Describe your logic which will be run each time when keyboard is about to be closed.
-      alert('hideee');
-    });
-
-    window.addEventListener('keyboardWillShow', function () {
-      // Describe your logic which will be run each time when keyboard is about to be closed.
-      alert('showww');
-    });
-
-
-    document.addEventListener('document - keyboardWillHide', function () {
-      // Describe your logic which will be run each time when keyboard is about to be closed.
-      alert('hideee');
-    });
-
-    document.addEventListener('document - keyboardWillShow', function () {
-      // Describe your logic which will be run each time when keyboard is about to be closed.
-      alert('showww');
-    });
-  }
+  ngOnInit() {}
 }
