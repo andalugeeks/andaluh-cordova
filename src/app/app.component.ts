9dba76988a224e208c1b015ba60e50ee735991ce
Eduardo Amador Patón <eamadorpaton@gmail.com>
2019-03-01 11:18:53 +0100

    Lock portrait orientation for mobile devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+var screen: any = window.screen;
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,23 @@ export class AppComponent implements OnInit {
 
   constructor(translate: TranslateService) {
     translate.setDefaultLang('and');
+
+    this.deviceReady().then((device) => {
+      if (window.outerWidth < 768) {
+        // mobile - lock orientation to portrait
+        screen.orientation.lock('portrait');
+      }
+    })
   }
 
   ngOnInit() {}
+
+  deviceReady(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      document.addEventListener("deviceready", (device: any) => {
+        resolve(device);
+      }, false);
+    });
+  }
+
 }
